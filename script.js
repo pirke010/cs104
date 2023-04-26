d2ed462730a7cdec9959b27b881fab40b87c470c
Milan Stankovic <milanst.soko@gmail.com>
2023-04-26 21:10:06 +0200

    pomeranje_loptice

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 "use strict;"
 
-// Creating the blocks
-
 const container = document.querySelector('#blocks');
 const board = document.querySelector('#board');
+const ball = document.querySelector('#ball');
+let x_ball = ball.offsetLeft; //
+let y_ball = ball.offsetTop;  // x and y coordinates of the ball
 let x_board = board.offsetLeft; // x-coordinate of the board
 let boardwidth = board.offsetWidth; // width of the board
+let stop = false // game-over indicator
 const blocks = [];
 
-document.addEventListener('keydown', move);
+let dx = 1
+let dy = -1
 
-for (let k=0; k<248; ++k) {
+document.addEventListener('keydown', board_move);
+document.addEventListener('keyup', start);
+
+for (let k=0; k<248; ++k) {// Creating the blocks
     const block = document.createElement('div');
     block.classList.add('tobreak');
     blocks[k] = block;
     container.appendChild(block);
 }
 
-function move(event) {
+function board_move(event) {
     if (event.code == 'ArrowLeft') {
         if (x_board > boardwidth/2 + 2) {
             x_board = x_board - 2
@@ -30,4 +36,29 @@ function move(event) {
             board.style.left = `${x_board}px`
         }
     }
+}
+
+function start(event) {
+    if (event.code == 'Space') { //press space to start
+        requestAnimationFrame(ball_move)
+    }
+}
+function ball_move() {
+    if (x_ball < ball.offsetWidth | x_ball > window.innerWidth - ball.offsetWidth){ //vertical wall hit
+        dx = -dx ; 
+    }
+    if (y_ball < 2*ball.offsetHeight){ //ceiling hit
+        dy = -dy ; 
+    }
+    if (y_ball > window.innerHeight - ball.offsetHeight/2) {
+        stop = true
+    }
+    x_ball = x_ball + dx;
+    y_ball = y_ball + dy;
+    ball.style.left = `${x_ball}px`;
+    ball.style.top = `${y_ball}px`;
+    if (!stop){
+        requestAnimationFrame(ball_move)
+    }
+    
 }
