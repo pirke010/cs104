f8a5766d9e86b03bcdac823becd85d8081d0f8c0
Milan Stankovic <milanst.soko@gmail.com>
2023-05-04 23:22:03 +0200

    svasta

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict;"
-const m1 = 4;
-const n1 = 10;
+
+const m1 = 6;
+const n1 = 12;
 const w1 = 1;
 
 let m = m1;
@@ -8,76 +9,82 @@ let n = n1;
 let w = w1;
 let score = 0;
 
-
-const body = document.querySelector('#container');
+const container = document.querySelector('#container');
 const board = document.querySelector('#board');
 const ball = document.querySelector('#ball');
 const scoreboard = document.querySelector('#score');
+const levels = document.querySelector("#levels");
 
 const playAgain = document.querySelector('#again');
 playAgain.addEventListener('click', startAgain);
 const nextLevel = document.querySelector('#nextlevel');
 nextLevel.addEventListener('click', newLevel);
 
-scoreboard.style.left = '30px';
-scoreboard.style.top = `${window.innerHeight - 45}px`;
-
 const blocks = [];
 let colors = ['cyan', 'darkcyan', 'darkkhaki', 'darksalmon', 'darkolivegreen', 'darkorange', 'indiared', 'crimson', 'red', 'darkred'];
 
 let x_ball, y_ball, x_board, boardwidth, stop;
 
-let dx = 0;
-let dy = -5;
+let dx = 2;
+let dy = -2;
 let v = Math.sqrt(dx*dx + dy*dy); // speed
 
 let remaining = 0; // blocks remaining
-    
-container.addEventListener('mousemove', board_move);
-document.addEventListener('keyup', start);
+let level_ind; // level indicator
 
+board.style.left = `${window.innerWidth/2 - board.offsetWidth/2}px`;
+board.style.top = `${window.innerHeight - board.offsetHeight - 50}px`;
+ball.style.left = `${window.innerWidth/2 - ball.offsetWidth/2}px`;
+//ball.style.top = `${window.innerHeight - board.offsetHeight - 250 - ball.offsetHeight}px`;
+ball.style.top = `${window.innerHeight - board.offsetHeight - 50 - ball.offsetHeight}px`;
+scoreboard.style.left = '30px';
+scoreboard.style.top = `${window.innerHeight - 45}px`;
+stop = false;
 
-// const levels = document.querySelector("#levels");
-// const level1 = document.querySelector("#first");
-// const level2 = document.querySelector("#second");
-// const level3 = document.querySelector("#third");
-// const level4 = document.querySelector("#fourth");
-// const level5 = document.querySelector("#fifth");
-// const level6 = document.querySelector("#sixth");
-// const level7 = document.querySelector("#seventh");
-// const level8 = document.querySelector("#eight");
-// const level9 = document.querySelector("#ninght");
+for (let i = 0; i < 5; i++) {
+    const newl = document.createElement('div');
+    newl.classList.add("level");
+    newl.textContent = `level ${i+1}`;
+    newl.addEventListener('click', function() { 
+        setup(i);
+    } );
+    levels.appendChild(newl);
+}
+levels.style.left = `${window.innerWidth/2 - levels.offsetWidth/2}px`;
 
-// level1.addEventListener('click', firstlevel);
-
-// function firstlevel(event){
-//     simpleconfig(m, n, w);
-//     document.addEventListener('keyup', start);
-//     levels.classList.add('invisible');
-// }
-
-simpleconfig(m, n, w);
-
-function simpleconfig(m, n, w) {  //arranging blocks
+function setup(i) {
     blocks.length = 0;
     board.style.left = `${window.innerWidth/2 - board.offsetWidth/2}px`;
     board.style.top = `${window.innerHeight - board.offsetHeight - 50}px`;
     ball.style.left = `${window.innerWidth/2 - ball.offsetWidth/2}px`;
     //ball.style.top = `${window.innerHeight - board.offsetHeight - 250 - ball.offsetHeight}px`;
     ball.style.top = `${window.innerHeight - board.offsetHeight - 50 - ball.offsetHeight}px`;
-
     x_ball = ball.offsetLeft; // 
     y_ball = ball.offsetTop;  // x and y coordinates of the ball
     x_board = board.offsetLeft; // x-coordinate of the board
     boardwidth = board.offsetWidth; // width of the board
+    level_ind = i;
+    levels.classList.add('invisible');
+    container.addEventListener('mousemove', board_move);
+    document.addEventListener('keyup', start);
+    dx = 2;
+    dy = -2;
     stop = false; // game-over indicator
+    if (i < 2) {
+        simpleconfig(m, n, i+1);
+    }
+    else {
+        config1(m, n, i)
+    }
+}
 
+function simpleconfig(m, n, w) {  //arranging blocks
     for (let k = 0; k < m; k++) {
         for (let j = 0; j < n; j++) {
             const block = document.createElement('div');
             block.classList.add('tobreak1');
             block.style.backgroundColor = colors[w-1];
-            block.style.width = `${100/n - 0.1*n/2}%`;
+            block.style.width = `${(container.offsetWidth-6)/(n)-2}px`;
             if (k % 2 == j % 2) {
                 block.style.visibility = 'hidden';
                 blocks.push([block, 0, 0]);
@@ -86,26 +93,50 @@ function simpleconfig(m, n, w) {  //arranging blocks
                 blocks.push([block, w, w]);
                 remaining += 1;
             }
-            body.appendChild(block);
+            container.appendChild(block);
+        }
+    }
+}
+
+function config1(m, n, w) {
+    for (let k = 0; k < m; k++){
+        for (let j = 0; j < n; j++) {
+            const block = document.createElement('div');
+            block.classList.add('tobreak1');
+            block.style.width = `${(container.offsetWidth-6)/(n)-2}px`;
+            if (k==0 | k==m-1 | j==0 | j==n-1) {
+                block.style.backgroundColor = colors[w+1];
+                blocks.push([block, w+2, w+2]);
+                remaining += 1;
+            }
+            else if (k < m/2 + 1 && k > m/2 - 2 && j < n/2 + 1 && j > n/2 - 2 ) {
+                block.style.visibility = 'hidden';
+                blocks.push([block, 0, 0]);
+            }
+            else {
+                block.style.backgroundColor = colors[w-1];
+                blocks.push([block, w, w]);
+                remaining += 1;
+            }
+            container.appendChild(block);
         }
     }
 }
 
 function board_move(event) {
-    x_board = event.clientX - boardwidth/2;
-    board.style.left = x_board + 'px';       
+    x_board = Math.min(Math.max(event.clientX - boardwidth/2, container.offsetLeft + 3), container.offsetLeft + container.offsetWidth - boardwidth -3);
+    board.style.left =  x_board + 'px';
 }
-
 
 function start(event) {
     if (event.code == 'Space') { //press space to start
+        document.removeEventListener('keyup', start);
         requestAnimationFrame(playing);
     }
 }
 
 function playing() {
     ball_move();
-    document.removeEventListener('keyup', start);
     if (stop) {
         gameover();
     }
@@ -118,10 +149,10 @@ function playing() {
 }
 
 function ball_move() {
-    if (x_ball <= window.innerWidth/2 - 350 | x_ball > window.innerWidth/2 - ball.offsetWidth + 350){ //vertical wall hit
+    if (x_ball <= window.innerWidth/2 - 350 | x_ball >= window.innerWidth/2 - ball.offsetWidth + 350){ //vertical wall hit
         dx = -dx ; 
     }
-    if (y_ball < 0){ //ceiling hit
+    if (y_ball <= container.offsetTop){ //ceiling hit
         dy = -dy ; 
     }
     if (y_ball > board.offsetTop - ball.offsetHeight && y_ball < board.offsetTop ) {//board hit
@@ -171,7 +202,7 @@ function ball_move() {
             }    
         }   
     }
-    if (y_ball >= window.innerHeight - ball.offsetHeight) {
+    if (y_ball >= container.offsetTop + container.offsetHeight - ball.offsetHeight) {
         stop = true;
     }
     x_ball = x_ball + dx;
@@ -190,21 +221,22 @@ function win() {
 }
 
 function startAgain(event){
+    remaining = 0;
     for (let i = 0; i < m*n; ++i){
-        body.removeChild(blocks[i][0]);
+        container.removeChild(blocks[i][0]);
     }
     document.querySelector('#failed').classList.add('invisible');
     scoreboard.textContent = `score: 0`;
-    document.addEventListener('keyup', start);
-    simpleconfig(m1, n1, w1);
+    score = 0;
+    setup(level_ind);
 }
 
 function newLevel(event){
     for (let i = 0; i < m*n; ++i){
-        body.removeChild(blocks[i][0]);
+        container.removeChild(blocks[i][0]);
     }
-    w = w+1;
+    remaining = 0;
     document.querySelector('#win').classList.add('invisible');
-    document.addEventListener('keyup', start);
-    simpleconfig(m, n, w);
+    level_ind +=1;
+    setup(level_ind);
 }
